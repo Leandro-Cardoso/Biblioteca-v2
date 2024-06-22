09fb75682aae451a949e2f3f3d530b5c638b3500
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-22 14:59:42 -0300

    Caracteristicas dos livros

--- book.py
+++ book.py
@@ -1,6 +1,12 @@
 class Book():
-    def __init__(self, title: str) -> None:
+    def __init__(self, title: str, author: str, date: str, theme: str, historical_context: str, description: str, location: str) -> None:
         self.title = title
+        self.author = author
+        self.date = date
+        self.theme = theme
+        self.historical_context = historical_context
+        self.description = description
+        self.location = location
 
 if __name__ == '__main__':
     pass
