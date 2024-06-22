ec227e9ce3f303984e1ff288bf1dc15f51663700
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-22 19:02:18 -0300

    Funcao de ordenar melhorada

--- file.py
+++ file.py
@@ -4,13 +4,19 @@ from sort import radix_sort
 from search import binary_search
 
 class File():
-    def __init__(self, name: str, dir: str = None, ext: str = 'json', encoding: str = 'UTF-8') -> None:
+    def __init__(self, name: str, dir: str = None, ext: str = 'json', encoding: str = 'UTF-8', sort_key: str = None) -> None:
         self.name = name
         self.dir = dir
         self.ext = ext
         self.encoding = encoding
         self.path = self.get_path()
         self.content = self.get_content()
+        # Definir chave:
+        if sort_key:
+            self.sort_key = sort_key
+        else:
+            self.sort_key = list(self.content[0])[0]
+        self.key_values = self.get_key_values()
         self.sort()
     def get_path(self) -> str:
         '''Pega o caminho do arquivo.'''
@@ -27,30 +33,42 @@ class File():
             return loads(content)
         except:
             return []
-    def sort(self, key: str = None) -> None:
+    def get_key_values(self) -> list[dict]:
+        '''Pega uma lista dos valores da chave principal.'''
+        values = []
+        for dictionary in self.content:
+            values.append(dictionary[self.sort_key])
+        return values
+    def sort(self) -> None:
         '''Ordena conteúdo do arquivo pelo valor da chave passada, se não tiver, é usada a primeira.'''
         if len(self.content) > 0:
-            # Definir chave:
-            if not key:
-                key = list(self.content[0])[0]
-            # Criar uma lista com os valores das chaves:
-            values = []
-            for dictionary in self.content:
-                values.append(dictionary[key])
             # Ordenar lista:
-            sorteds = radix_sort(values)
+            sorteds = radix_sort(self.key_values)
             # Coloca os dicionários em ordem a partir da lista ordenada:
             sorted_dicts = []
             for s in range(len(sorteds)):
-                for v in range(len(values)):
-                    if sorteds[s] == values[v]:
+                for v in range(len(self.key_values)):
+                    if sorteds[s] == self.key_values[v]:
                         sorted_dicts.append(self.content[v])
             self.content = sorted_dicts
+            self.key_values = sorteds
+    def add(self, content: dict) -> None:
+        '''Adiciona um novo elemento no arquivo.'''
+        pass
+    def remove(self, content: dict) -> None:
+        '''Remove um elemento do arquivo.'''
+        pass
     def save(self) -> None:
         '''Salva as alterações feitas no conteúdo.'''
         pass
 
 if __name__ == '__main__':
     file = File('admins', 'users')
+    new_admin = {
+        'username' : 'kely',
+        'password' : 'asmin'
+    }
+    file.add(new_admin)
     for c in file.content:
         print(c['username'])
+    print(file.key_values)
