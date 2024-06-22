aeac388ea20beb0d9b66a8f098ce8b26be5aa606
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-22 19:01:31 -0300

    Busca aproximada

--- search.py
+++ search.py
@@ -1,13 +1,17 @@
-def binary_search(arr:list, key:str) -> int:
+def binary_search(arr:list, key:str, approximate: bool = False) -> int:
     '''Algoritmo de busca binária.'''
     s = 0
     e = len(arr) - 1
-    while e > 0:
+    while s <= e:
         m = (s + e) // 2
+        print(m)
         if arr[m] == key:
             return m
         elif arr[m] < key:
             s = m + 1
         else:
             e = m - 1
-    return -1
+    if approximate:
+        return m
+    else:
+        return -1
