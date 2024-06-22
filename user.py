e6b1b2a69be99ce34668c32d0a4e314ed9186ab4
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-22 15:15:29 -0300

    Caracteristicas do autor

--- user.py
+++ user.py
@@ -3,6 +3,14 @@ class User():
         self.username = username
         self.password = password
 
+class Author(User):
+    def __init__(self, username: str, password: str, name: str, date_birth: str, biography: str, research_areas: list) -> None:
+        super().__init__(username, password)
+        self.name = name
+        self.date_birth = date_birth
+        self.biography = biography
+        self.research_areas = research_areas
+
 class Librarian(User):
     def __init__(self, username: str, password: str) -> None:
         super().__init__(username, password)
