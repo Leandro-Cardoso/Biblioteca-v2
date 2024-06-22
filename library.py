57306b6d1eb0130d2f49af215c3fffc1efaf908c
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-22 15:51:03 -0300

    Definicao das funcoes da biblioteca

--- library.py
+++ library.py
@@ -1,15 +1,24 @@
+from document import Document
+from event import Event
+from itinerary import Itinerary
+
 class Library():
     def __init__(self, name: str) -> None:
         self.name = name
         self.admins = self.get_admins()
         self.librarians = self.get_librarians()
-        self.books = self.get_books()
+        self.documents = self.get_documents()
     def get_admins(self) -> list:
         '''Retorna todos os admins da biblioteca.'''
         pass
     def get_librarians(self) -> list:
         '''Retorna todos os bibliotecários.'''
         pass
-    def get_books(self) -> list:
-        '''Retorna todos os livros registrados.'''
+    def get_documents(self) -> list:
+        '''Retorna todos os documentos registrados.'''
         pass
+    def lend_document(self, document: Document, event: Event) -> None:
+        '''Empresta um determinado documento para um determinado evento.'''
+        pass
+    def schedule_tour(self, itinerary: Itinerary) -> None:
+        '''Agenda uma visita guiada, com um roteiro em específico.'''
--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from library import Library
 from user import User, Author, Librarian, Admin
 from document import Document
 
-# Criar sistema de empréstimo de documentos para eventos e exposições.
-# Agendar visitas guiadas.
-
 def main() -> None:
     library = Library()
 
