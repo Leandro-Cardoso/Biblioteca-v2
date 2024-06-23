ec85c8d4ab8fa3caa15cc6fe1c241a856cf186d3
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-06-22 22:08:14 -0300

    Criar arquivo caso nao exista

--- file.py
+++ file.py
@@ -9,15 +9,22 @@ class File():
         self.dir = dir
         self.ext = ext
         self.encoding = encoding
+        self.sort_key = sort_key
         self.path = self.get_path()
-        self.content = self.get_content()
-        # Definir chave:
-        if sort_key:
-            self.sort_key = sort_key
+        if path.exists(dir):
+            self.content = self.get_content()
+            # Definir chave:
+            if not sort_key:
+                self.sort_key = list(self.content[0])[0]
+            self.key_values = self.get_key_values()
+            # Ordenar conteúdo:
+            self.sort()
         else:
-            self.sort_key = list(self.content[0])[0]
-        self.key_values = self.get_key_values()
-        self.sort()
+            self.content = []
+            self.key_values = []
+            # Criar diretório:
+            makedirs(dir)
+            self.save()
     def get_path(self) -> str:
         '''Pega o caminho do arquivo.'''
         filename = f'{self.name}.{self.ext}'
@@ -32,16 +39,18 @@ class File():
                 content = file.read()
             return loads(content)
         except:
+            self.sort_key = None
             return []
     def get_key_values(self) -> list[dict]:
         '''Pega uma lista dos valores da chave principal.'''
         values = []
         for dictionary in self.content:
+            print(dictionary, self.sort_key)
             values.append(dictionary[self.sort_key])
         return values
     def sort(self) -> None:
         '''Ordena conteúdo do arquivo pelo valor da chave passada, se não tiver, é usada a primeira.'''
-        if len(self.content) > 0:
+        if len(self.content) > 0 and self.sort_key:
             # Ordenar lista:
             sorteds = radix_sort(self.key_values)
             # Coloca os dicionários em ordem a partir da lista ordenada:
@@ -54,30 +63,50 @@ class File():
             self.key_values = sorteds
     def add(self, dictionary: dict) -> None:
         '''Adiciona um novo elemento no arquivo.'''
+        # Definir chave:
+        if not self.sort_key:
+            self.sort_key = list(dictionary)[0]
+        # Buscar:
         i = binary_search(self.key_values, dictionary[self.sort_key])
         if i == -1:
+            # Adicionar:
             self.content.append(dictionary)
             self.key_values.append(dictionary[self.sort_key])
             self.sort()
         else:
+            # Sobrescrever:
             self.content[i] = dictionary
     def remove(self, dictionary: dict) -> None:
         '''Remove um elemento do arquivo.'''
-        i = binary_search(self.key_values, dictionary[self.sort_key])
-        if i != -1:
-            del self.content[i]
-            del self.key_values[i]
+        if self.sort_key:
+            i = binary_search(self.key_values, dictionary[self.sort_key])
+            if i != -1:
+                del self.content[i]
+                del self.key_values[i]
+        # Definir chave:
+        if len(self.content) == 0:
+            self.sort_key = None
     def save(self) -> None:
         '''Salva as alterações feitas no conteúdo.'''
-        pass
+        with open(self.path, 'w', encoding = self.encoding) as file:
+            file.write(dumps(self.content))
 
 if __name__ == '__main__':
     file = File('admins', 'users')
     user = {
         'username' : 'xuxa',
-        'password' : 'asmin'
+        'password' : 'admin'
     }
     file.remove(user)
     for c in file.content:
         print(c['username'])
     print(file.key_values)
+    file.save()
+
+    doc = File('documents', 'documents')
+    document = {
+        'title' : 'harry potter',
+        'author' : 'j k rowling'
+    }
+    doc.add(document)
+    doc.save()
